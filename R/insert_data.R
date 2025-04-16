846802034d64a7973ebbd9e66b3c6a516faf7042
Shaun Harrington <81801943+sharrington92@users.noreply.github.com>
2025-04-15 23:32:36 -0700

    Begin creating function to get schools table data

--- R/insert_data.R
+++ R/insert_data.R
@@ -151,7 +151,109 @@ populate_schools <- function(con){
 
   status_out("\nPopulating schools...", "start")
 
+  # Get any school page
+  u <- "https://stats.ncaa.org/teams/history?utf8=%E2%9C%93&org_id=260&sport_code=MBB&commit=Search"
 
+  parsed.html <- parse_webpage(u)
+
+  # Select the <select> element with id "year_list"
+  school.select <- html_node(parsed.html, "select#org_id_select")
+
+  # Extract all <option> nodes within the selected <select>
+  option_nodes <- html_nodes(school.select, "option")
+
+  # Extract the 'value' attributes
+  school.ids <- html_attr(option_nodes, "value")
+
+  # Extract the text labels and trim any leading/trailing whitespace
+  schools <- html_text(option_nodes) %>% trimws()
+
+  df.schools <-
+    tibble(
+      school_id = as.integer(school.ids)[-1],
+      ncaa_name = schools[-1]
+    ) #%>%
+    # left_join(
+    #   y = ncaahoopR::ncaa_colors %>%
+    #     mutate(
+    #       ncaa_name = case_when(
+    #         ncaa_name == "Fairleigh Dickinson" ~ "FDU",
+    #         ncaa_name == "IUPUI" ~ "IU Indy",
+    #         ncaa_name == "Saint Francis (PA)" ~ "Saint Francis",
+    #         is.character(ncaa_name) ~ ncaa_name
+    #       )
+    #     ) %>%
+    #     left_join(
+    #       y = ncaahoopR::ids %>%
+    #         rename(
+    #           espn_id = id,
+    #           espn_link = link
+    #         ),
+    #       by = c("espn_name" = "team")
+    #     ),
+    #   by = "ncaa_name"
+    # ) %>%
+    # select(-c(color_3, conference))
+
+  # Create list of tables with school names
+  school.tables <- list(
+    df.schools,
+    ncaa_colors |> select(ncaa_name, espn_name),
+    ncaahoopR::ids |> select(team, espn_abbrv),
+    ncaahoopR::dict |> select(-conference)
+  )
+
+  # Iterate through each dataset creating inner and anti joins
+  # with each column to get a accumulating match from the scraped names
+  # with those in ncaahoopR package.
+  joins <- list()
+  index <- 1
+  for(t in seq_along(school.tables)[-1]){
+    df <- school.tables[[t]]
+    for(i in seq_along(colnames(df))){
+      if(index == 1){
+        joins[[index]] <- school.tables[[1]] |>
+          select(school_id, ncaa_name) |>
+          inner_join(
+            df,
+            join_by(ncaa_name == !!sym(colnames(df)[i]))
+          )
+      } else if(i == 1){
+        joins[[index]] <- do.call(bind_rows, joins) |>
+          select(school_id, ncaa_name) |>
+          inner_join(
+            df,
+            join_by(ncaa_name == !!sym(colnames(df)[i]))
+          )
+      } else{
+        joins[[index]] <-
+          # joins[[index-1]] |>
+          do.call(bind_rows, joins) |>
+          select(school_id, ncaa_name) |>
+          anti_join(df, join_by(ncaa_name == !!sym(colnames(df)[i-1]))) |>
+          inner_join(
+            df,
+            join_by(ncaa_name == !!sym(colnames(df)[i]))
+          )
+      }
+      index = index + 1
+    }
+  }
+
+  # Bind joins together, and get distinct matches for each scraped name
+  joins.df <- do.call(bind_rows, purrr::discard(joins, ~nrow(.x) == 0)) |>
+    distinct() |>
+    tidyr::pivot_longer(-c(school_id, ncaa_name), values_drop_na = TRUE) |>
+    distinct() |>
+    tidyr::pivot_wider(names_from = name, values_from = value)
+
+  # ncaahoopR::ids # espn ids
+  # ncaahoopR::conf # conferences
+
+
+
+  df.schools %>%
+    dbAppendTable(duck.con, "schools", .)
 
   status_out("Complete", "done", width = NULL)
 }
