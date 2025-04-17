98e8713b245b80932953e48e08a38de518e76139
Shaun Harrington <81801943+sharrington92@users.noreply.github.com>
2025-04-16 22:07:48 -0700

    Added option to create an in-memory database for dev purposes

--- R/init_dunkdb.R
+++ R/init_dunkdb.R
@@ -4,17 +4,26 @@
 #' *dunkdb*() creates a DuckDB database and the necessary database objects
 #' to store NCAA men's basketball data in a normalized relational model.
 #'
-#' @param path File path for database location.
+#' @param path File path for database location. If path = "" or
+#' path = ":memory:", an in-memory database instance is created.
 #' @param db_name Name of DuckDB database.
 #'
 #' @returns A DuckDB database connection.
 #' @export
 #'
+#' @example
+#' init_dunkdb()
 init_dunkdb <- function(path = getwd(), db_name = "dunkdb.ddb"){
+
+  if(path == "" | path == ':memory:') {
+    db_full_path <- ':memory:'
+  } else{
+    db_full_path <- file.path(path, db_name)
+  }
 
   # Create DuckDB Instance if needed
   ddb <- duckdb::duckdb(
-    dbdir = file.path(path, db_name),
+    dbdir = db_full_path,
     read_only = FALSE
   )
 
